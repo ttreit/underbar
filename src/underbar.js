ee23883e4d48de87bd8d1586d055905dc55eab6b
ttreit <14933277+ttreit@users.noreply.github.com>
2020-01-12 20:53:20 -0800

    Add command to return last elements of an array
    All working except does not return empty array if passed zero.

--- src/underbar.js
+++ src/underbar.js
@@ -37,8 +37,10 @@
 
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
-  _.last = function(array, n) {
-  };
+
+_.last = ((array, n) => (n === undefined ? array[array.length-1] : array.slice(n * -1)));
+  // _.last = function(array, n) {
+  // };
 
   // Call iterator(value, key, collection) for each element of collection.
   // Accepts both arrays and objects.
